64484f7c26b98016d58fdd41ffe030e7111d2d17
lingxt <lxt9601@gmail.com>
2019-08-29 18:31:01 +0900

    add API 'getpost'

--- app/controllers/muscle_posts_controller.rb
+++ app/controllers/muscle_posts_controller.rb
@@ -11,18 +11,52 @@ class MusclePostsController < ApplicationController
     # データを取得
     result = MusclePost.where(id: muscle_post_id_ary)
                  .joins(tag_maps: :body_part)
-                 .select("muscle_posts.*, body_parts.name")
                  .joins(:user)
                  .select("users.icon, users.identity, users.name, muscle_posts.*, body_parts.name")
 
     # 整形
+    temp_hash = data_formation(result)
+    responses = []
+    for muscle_post_id in muscle_post_id_ary
+      responses.push(temp_hash[muscle_post_id])
+    end
+
+    # レスポンスを返す
+    render :json => responses.to_json
+  end
+
+  def getpost
+    # 要求数取得
+    muscle_post_id = params[:id]
+
+    # データを取得
+    result = MusclePost.where(id: muscle_post_id)
+                 .joins(tag_maps: :body_part)
+                 .joins(:user).select("users.icon, users.identity, users.name, muscle_posts.*, body_parts.name")
+
+    # データ存在しないとき、[]を返す
+    if result.blank?
+      render :json => []
+      return
+    end
+
+    # 整形
+    temp_hash = data_formation(result)
+    responses = [temp_hash[muscle_post_id.to_i]]
+
+    # レスポンスを返す
+    render :json => responses.to_json
+  end
+
+  private
+  # 整形
+  def data_formation(result)
     temp_hash = {}
     for r in result
-     if temp_hash.has_key?(r.id)
+      if temp_hash.has_key?(r.id)
         temp_hash[r.id]['body_parts'].push(r.name)
         next
       end
-      temp_hash[r.id] = {}
       temp_hash[r.id] = {}
       temp_hash[r.id]['icon'] = r.icon
       temp_hash[r.id]['identity'] = r.identity
@@ -31,13 +65,6 @@ class MusclePostsController < ApplicationController
       temp_hash[r.id]['body'] = r.body
       temp_hash[r.id]['body_parts'] = [r.name]
     end
-
-    responses = []
-    for muscle_post_id in muscle_post_id_ary
-      responses.push(temp_hash[muscle_post_id])
-    end
-
-    # レスポンスを返す
-    render :json => responses.to_json
+    temp_hash
   end
 end
