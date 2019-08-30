6e8499f1d4739ead98a895e8c1c1d171bad4feeb
yuu <ozaki-yu-ck@ynu.jp>
2019-08-30 21:43:30 +0900

    レスポンス,routes.rb,user.rbの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,49 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
+  def get_user_data
     @user = User.find(params[:id])
-  end
 
-  def new
-    @user = User.new
+    if !@user.unsubscribed
+      # 退会済みでない時にjsonを返す
+      render status: 200, :json => @user
+    else
+      render status: 400
+    end
   end
 
   def edit
+    # 現在のプロフィールを返す
     @user = User.find(params[:id])
+    render :json => @user
   end
 
   def create
+    # 入力されたパラメータで保存
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      render status: 200
     else
-      render 'new'
+      render status: 400
     end
-
    end
 
   def update
-    @user = User.find(params[:id])
+    # editの後で入力された値で更新
+    @user = User.find(user_params)
 
     if @user.update(user_params)
-      redirect_to @user
+      render status: 200
     else
-      render 'edit'
+      render status: 400
     end
   end
 
   def destroy
+    # 退会処理
     @user = User.find(params[:id])
+    @user.update(unsubsribed: true)
 
-    redirect_to new_user_path
+    render status: 200
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :identity, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
   has_many :muscle_posts
 end
