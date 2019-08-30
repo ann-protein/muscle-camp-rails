929f7d8a0f69aebff95925d3b7a5c36def4a8a4b
yuu <ozaki-yu-ck@ynu.jp>
2019-08-31 00:00:45 +0900

    パスワードを半角英数のみに限定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   validates :identity, uniqueness: true
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 5 }
+  validates :HalfSize_only
   has_many :muscle_posts
+
+  def HalfSize_only
+    if password !~ /\A\w+\z/
+      errors.add(:password, " : パスワードは半角英数のみ")
+    end
+  end
+  
 end
