60dc96cce60ed0c6d8ea7ce08d451c28b4db8205
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2019-09-14 04:47:04 +0900

    fix email notnull and unique

--- db/migrate/20190830163827_add_token_to_users.rb
+++ db/migrate/20190830163827_add_token_to_users.rb
@@ -1,6 +1,11 @@
 class AddTokenToUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :users, :token, :string
+    change_column :users, :token, :string, null: false
     add_index :users, :token, unique: true
+  end
+
+  def down
+    remove_column :users, :token
   end
 end
